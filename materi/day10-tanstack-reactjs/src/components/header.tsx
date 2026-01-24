1bdbfab0919dad17561e5f016adc5c01274452bc
ammaar12 <muhammadfarroos12@gmail.com>
2026-01-24 11:04:06 +0700

    Add initial project files including robots.txt, .gitignore, and configuration for Vite and React

--- materi/day10-tanstack-reactjs/src/components/header.tsx
+++ materi/day10-tanstack-reactjs/src/components/header.tsx
@@ -1,13 +1,6 @@
 export function Header() {
 
     return (
-        <div className="bg-blue-900 text-white p-2 rounded-lg">
-            <div className="">
-                Ini adalah header
-            </div>
-            <div className="">
-                tessttt
-            </div>
-        </div>
+        <div className="bg-blue-900 text-white p-2 rounded-lg h-20"/>
     )
 }
