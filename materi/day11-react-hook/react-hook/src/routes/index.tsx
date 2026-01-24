bb21b90d2522f6c9a881e5e001aa1dbaf5aad2ea
ammaar12 <muhammadfarroos12@gmail.com>
2026-01-25 03:39:12 +0700

    Add admin check to login state management and display welcome message

--- materi/day11-react-hook/react-hook/src/routes/index.tsx
+++ materi/day11-react-hook/react-hook/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -9,11 +9,23 @@ function App() {
   // setState
   const [loginData, setLoginData] = useState({
     username: "",
-    password: ""
+    password: "",
+    isAdmin: false
   })
+
+  // digunakan untuk menjalankan fungsi secara langsung apabila ada perubahan dalam data yang ada dalam pantauan
+  useEffect(() => {
+    setLoginData(prev => ({
+      ...prev,
+      isAdmin:
+        prev.username === 'admin' &&
+        prev.password === 'admin'
+    }))
+  }, [loginData.username, loginData.password])
+
   return (
     <div className='flex justify-center items-center h-screen bg-amber-50'>
-      <div className='w-[300px]'>
+      <div className='w-[300px] space-y-4'>
         <input
           onChange={(event) => { setLoginData({ ...loginData, username: event.target.value }) }}
           className='p-2 rounded-lg border'
@@ -26,6 +38,7 @@ function App() {
 
         <div>Username: {loginData.username}</div>
         <div>Password: {loginData.password}</div>
+        <div className='text-4xl'>{loginData.isAdmin ? "welcome admin" : null}</div>
       </div>
     </div>
   )
