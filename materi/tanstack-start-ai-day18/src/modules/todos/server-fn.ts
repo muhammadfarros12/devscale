f7629df7084f345cf7685f097797841727d0d57b
ammaar12 <muhammadfarroos12@gmail.com>
2026-03-04 15:36:34 +0700

    Add Prisma setup with SQLite, create Todo model, and implement server function for note creation

--- materi/tanstack-start-ai-day18/src/modules/todos/server-fn.ts
+++ materi/tanstack-start-ai-day18/src/modules/todos/server-fn.ts
@@ -1,7 +1,14 @@
 import { createServerFn } from "@tanstack/react-start";
+import { prisma } from "#/utils/prisma";
 
 export const createNote = createServerFn({ method: "POST" })
 	.inputValidator((data: { input: string }) => data)
-	.handler(({ data }) => {
-		console.log(data.input);
+	.handler(async ({ data }) => {
+		const newTodo = await prisma.todo.create({
+			data: {
+				content: data.input,
+			},
+		});
+
+		console.log(newTodo);
 	});
