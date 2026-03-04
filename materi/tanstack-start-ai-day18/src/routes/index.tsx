fb8d0182521ce0ec1567317986554da3ecfd812b
ammaar12 <muhammadfarroos12@gmail.com>
2026-03-04 15:58:09 +0700

    Implement note creation with dynamic input; add text input for note content

--- materi/tanstack-start-ai-day18/src/routes/index.tsx
+++ materi/tanstack-start-ai-day18/src/routes/index.tsx
@@ -1,15 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { createNote } from "#/modules/todos/server-fn";
+import { useState } from "react";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
+	const [content, setContent] = useState("");
 	async function handleCreateNote() {
-		await createNote({ data: { input: "Hello World" } });
+		await createNote({ data: { input: content } });
 	}
 
 	return (
 		<div>
+			<input type="text" onChange={(e) => setContent(e.target.value)} />
 			<button type="button" onClick={handleCreateNote}>
 				Hello
 			</button>
